e3e93c405a829ecab4700831175dfa396a1f59b3
jhermann <jh@web.de>
2013-08-30 02:25:01 +0200

    option -n; comments

--- README.md
+++ README.md
@@ -7,5 +7,5 @@ mkdir -p "$HOME/src/github"
 cd "$HOME/src/github"
 git clone "git@github.com:jhermann/ruby-slippers.git"
 cd "ruby-slippers"
-./click-click-click.sh
+./click-click-click.sh -n
 ```
--- click-click-click.sh
+++ click-click-click.sh
@@ -1,29 +1,41 @@
 #! /bin/bash
+#
+# Link version-controlled home skeleton into user account-
+#
 
 DRY=
-#DRY=:
+test "$1" = "-n" && DRY=:
+
+set -e
 
 root=$(cd $(dirname "$0") && pwd)
 linkbase="$root/home"
 
 ( cd "$linkbase" && find . -type f ) | while read file; do
+    # omit "./"
     file="${file:2}"
+
+    # skip (editor) junk
     case "$file" in
         *~ | *.swp | *.tmp) echo "*** Ignoring $file" ; continue ;;
     esac
 
+    # try to link file
     dstdir=$(dirname ~/"$file")
     test -d "$dstdir" || $DRY mkdir -p "$dstdir"
     if test -h ~/"$file"; then
-        test $(readlink ~/"$file") = "$linkbase/$file" || echo ~/"$file" "<==" "$linkbase/$file"
+        # symlinked
+        test $(readlink ~/"$file") = "$linkbase/$file" || echo ~/"$file" "=->" "$linkbase/$file"
         $DRY ln -nfs "$linkbase/$file" ~/"$file"
     elif test -f ~/"$file"; then
+        # conflict
         echo
         diff -U1 ~/"$file" "$linkbase/$file"
         echo rm ~/"$file" "# to get this file out of the way"
         echo
     else
-        echo ~/"$file" "<++" "$linkbase/$file"
+        # missing
+        echo ~/"$file" "+->" "$linkbase/$file"
         $DRY ln -s "$linkbase/$file" ~/"$file"
     fi
 done
