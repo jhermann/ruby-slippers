216ad0847af60e79fbb2f0e051da64e8f1e3eed1
jhermann <jh@web.de>
2013-09-11 19:12:46 +0200

    Basic preferences

---
+++ home/.bashrc.d/basics.sh
@@ -0,0 +1,20 @@
+#
+# Basic preferences
+#
+
+umask 0027
+export EDITOR=vi
+grep ~/bin: <<<"$PATH" >/dev/null || export PATH=~/bin:"$PATH"
+
+export AUTOSSH_MAXSTART=666
+export AUTOSSH_PORT=61443
+
+
+# Helpers
+imgur() {
+    for i in "$@";do
+        curl -# -F "image"=@"$i" -F "key"="4907fcd89e761c6b07eeb8292d5a9b2a" http://imgur.com/api/upload.xml | \
+            grep -Eo '<[a-z_]+>http[^<]+'|sed 's/^<.\|_./\U&/g;s/_/ /;s/<\(.*\)>/\x1B[0;34m\1:\x1B[0m /'
+    done
+}
+
