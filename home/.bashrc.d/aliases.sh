66e17d493aa24def3c54b40ced8e34bca078c8fe
jhermann <jh@web.de>
2013-09-11 19:11:00 +0200

    more aliases

--- home/.bashrc.d/aliases.sh
+++ home/.bashrc.d/aliases.sh
@@ -1,7 +1,22 @@
-#! /bin/sh
+#
+# bash alias definitions
+#
 alias dir="ls -F"
 alias l="ls -l"
 alias la="ls -la"
 alias md=mkdir
 alias ..="cd .."
 alias ...="cd ../.."
+
+alias rebashrc=". ~/.bashrc"
+
+alias cdiff='colordiff | less -r'
+alias svndiffb='svn diff --diff-cmd diffb | cdiff '
+alias svnignore="svn propedit svn:ignore"
+
+alias total="awk '{tot=tot+\$1} END {print tot}'"
+alias avg="awk '{sum+=\$1} END { print sum/NR}'"
+
+# Typos
+alias gti=git
+
