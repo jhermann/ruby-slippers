3961d63bca7e01881c9d55c528e0ea2a9367ebda
jhermann <jh@web.de>
2013-10-23 16:42:39 +0200

    Protect against failures of bashv4 stuff in older shells

--- home/.bashrc.d/bash4.sh
+++ home/.bashrc.d/bash4.sh
@@ -1,8 +1,10 @@
-# for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
+# For setting history length, see HISTSIZE and HISTFILESIZE in bash(1)
 HISTSIZE=1000
 HISTFILESIZE=2000
 
-# If set, the pattern "**" used in a pathname expansion context will
-# match all files and zero or more directories and subdirectories.
-shopt -s globstar
-
+# Protect against failures of bashv4 stuff in older shells
+if test "${BASH_VERSINFO[0]}" -ge 4 ; then
+    # If set, the pattern "**" used in a pathname expansion context will
+    # match all files and zero or more directories and subdirectories.
+    shopt -s globstar
+fi
