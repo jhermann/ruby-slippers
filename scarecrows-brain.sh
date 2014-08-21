e603caa353914e4c3c64af07ae47fc74de6e6fd4
jhermann <jh@web.de>
2014-08-21 12:50:39 +0200

    install hg and git-remote-hg

--- scarecrows-brain.sh
+++ scarecrows-brain.sh
@@ -6,6 +6,7 @@ set -e
 scriptdir="$(cd "$(dirname "$0")" && pwd)"
 venvdir="$HOME/.pyvenv/ruby-slippers"
 tmpbase="/tmp/$(basename "$0")-$USER-$$"
+git_remote_hg_url="https://raw.github.com/felipec/git-remote-hg/master/git-remote-hg"
 action="$1"; shift || :
 
 
@@ -25,8 +26,11 @@ pip_install() {
 
 
 main() {
-    tools=""
+    # Make venv
     pyvenv
+
+    # Install tools into venv
+    tools=""
     pip_install "yolk" || :
     pip_install "pylint>=1.0"; tools="$tools pyreverse epylint pylint pylint-gui symilar"
     pip_install "flake8"; tools="$tools pyflakes pep8 flake8"
@@ -37,14 +41,22 @@ main() {
     pip_install "docutils >= 0.11"; tools="$tools rst2xml.py rst2s5.py rst2odt.py rst2man.py rst2latex.py rst2html.py"
     pip_install "Sphinx == 1.1.3"; tools="$tools sphinx-quickstart sphinx-build sphinx-autogen sphinx-apidoc"
     pip_install "https://github.com/jhermann/nodeenv/archive/master.zip#egg=nodeenv"; tools="$tools nodeenv"
+    pip_install "mercurial"; tools="$tools hg"
 
+    # Link selected tools into ~/bin
     mkdir -p ~/bin
     for tool in $tools; do
         target=${tool%.py}
         echo "Symlinking ~/bin/$target"
         ln -nfs "../.pyvenv/ruby-slippers/bin/$tool" "$HOME/bin/$target"
     done
-    ls -lrt "$HOME/.pyvenv/ruby-slippers/bin/" | tail -n10
+    ls -lrt "$venvdir/bin" | tail -n10
+
+    # Install git-remote-hg
+    if test ! -x ~/bin/git-remote-hg; then
+        { echo "#! $venvdir/bin/python"; curl -skSL "$git_remote_hg_url"; } > ~/bin/git-remote-hg
+        chmod +x ~/bin/git-remote-hg
+    fi
 }
 
 
