b2c13c30f6a0105d12dd1e7ce679b1aad1de27db
jhermann <jh@web.de>
2014-08-27 12:43:34 +0200

    Activate "pipsi" installs

--- home/.bashrc.d/python.sh
+++ home/.bashrc.d/python.sh
@@ -4,3 +4,7 @@
 
 alias pyzen="python -c 'import this'"
 
+# Activate "pipsi" installs
+if test -d ~/.local/bin && grep -v ":$HOME/.local/bin:" <<<":$PATH:" >/dev/null; then
+    export PATH="$HOME/.local/bin:$PATH"
+fi
