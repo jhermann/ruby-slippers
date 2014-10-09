aee2e928f153aa0f57742da7a8fa7cd7c297c53f
jhermann <jh@web.de>
2014-10-09 14:21:16 +0200

    'notes' shortcut, and section comments

--- home/.bashrc.d/aliases.sh
+++ home/.bashrc.d/aliases.sh
@@ -1,6 +1,8 @@
 #
 # bash alias definitions
 #
+
+# Shortcuts
 alias dir="ls -F"
 alias l="ls -l"
 alias la="ls -la"
@@ -8,15 +10,17 @@ alias md=mkdir
 alias ..="cd .."
 alias ...="cd ../.."
 alias xo=xdg-open
+alias notes="pe $HOME/Documents/notes.md"
 
+# Little helpers
 alias rebashrc=". ~/.bashrc"
+alias total="awk '{tot=tot+\$1} END {print tot}'"
+alias avg="awk '{sum+=\$1} END { print sum/NR}'"
 
+# SCM
 alias cdiff='colordiff | less -r'
 alias svndiffb='svn diff --diff-cmd diffb | cdiff '
 alias svnignore="svn propedit svn:ignore"
-
-alias total="awk '{tot=tot+\$1} END {print tot}'"
-alias avg="awk '{sum+=\$1} END { print sum/NR}'"
 
 # Typos
 alias gti=git
