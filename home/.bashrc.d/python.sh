a3367f5c1b5408b2734c43c5bd7104f22113a528
jhermann <jh@web.de>
2015-03-13 00:59:25 +0100

    alias to install directly from PyPI

--- home/.bashrc.d/python.sh
+++ home/.bashrc.d/python.sh
@@ -4,6 +4,7 @@
 
 alias click-click-click='source ~/.pyvenv/ruby-slippers/bin/activate'
 alias pyzen="python -c 'import this'"
+alias pypi="pip install -i https://pypi.python.org/simple"
 
 # Activate "pipsi" installs
 if test -d ~/.local/bin && grep -v ":$HOME/.local/bin:" <<<":$PATH:" >/dev/null; then
