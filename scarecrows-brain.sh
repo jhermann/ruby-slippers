72bec36534894adc30b705de90a091e6db8e5979
jhermann <jh@web.de>
2015-03-22 16:08:43 +0100

    :new: install pypirc

--- scarecrows-brain.sh
+++ scarecrows-brain.sh
@@ -68,6 +68,7 @@ main() {
     tool_install "check-manifest"
     tool_install "howdoi"
     tool_install "clf"
+    tool_install "pypirc"
     # tool_install "joe"
     pip_install "ansible"; tools="$tools ansible ansible-doc ansible-galaxy ansible-playbook ansible-pull ansible-vault"
 
