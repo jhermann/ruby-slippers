800ed8966b0e7565f7d8b6a71093872ace32b298
jhermann <jh@web.de>
2015-03-30 06:09:29 +0200

    :arrow_upper_right: Make pydoc work in virtualenvs

--- home/.bashrc.d/python.sh
+++ home/.bashrc.d/python.sh
@@ -6,6 +6,10 @@ alias click-click-click='source ~/.pyvenv/ruby-slippers/bin/activate'
 alias pyzen="python -c 'import this'"
 alias pypi="pip install -i https://pypi.python.org/simple"
 
+# Make pydoc work in virtualenvs, and also alias the system pydoc
+alias pydoc="python -c 'import pydoc; pydoc.cli()'"
+alias spydoc="command pydoc"
+
 # Activate "pipsi" installs
 if test -d ~/.local/bin && grep -v ":$HOME/.local/bin:" <<<":$PATH:" >/dev/null; then
     export PATH="$HOME/.local/bin:$PATH"
