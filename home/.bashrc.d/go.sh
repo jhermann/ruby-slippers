bd635aa4673c3bbb70db6fe638cd3f3c20a2a1fa
jhermann <jh@web.de>
2015-09-10 13:10:15 +0200

    activate an installed golang 1.5.1

--- home/.bashrc.d/go.sh
+++ home/.bashrc.d/go.sh
@@ -6,3 +6,14 @@ export GOPATH=$HOME/lib/gocode
 
 export GIMME_VERSION_PREFIX="$HOME/.local/gimme/versions"
 export GIMME_ENV_PREFIX="$HOME/.local/gimme/envs"
+
+export GOVERSION=1.5.1
+if test -d ~/.gimme/versions/go${GOVERSION}.*; then
+    unset GOOS
+    unset GOARCH
+    export GOROOT="$HOME/.gimme/versions/go${GOVERSION}.linux.amd64"
+    export GIMME_ENV="$HOME/.gimme/envs/go${GOVERSION}.env"
+
+    grep ":$HOME/.gimme/versions/go${GOVERSION}.linux.amd64/bin:" <<<":$PATH:" >/dev/null \
+        || export PATH="$HOME/.gimme/versions/go${GOVERSION}.linux.amd64/bin:${PATH}"
+fi
