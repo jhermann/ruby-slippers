a24fbd10445a8792d250054baea3061f30d181a4
jhermann <jh@web.de>
2016-04-13 12:49:58 +0200

    utf8tolatin1 alias using iconv

--- home/.bashrc.d/aliases.sh
+++ home/.bashrc.d/aliases.sh
@@ -16,6 +16,7 @@ alias notes="pe $HOME/Documents/notes.md"
 alias rebashrc=". ~/.bashrc"
 alias total="awk '{tot=tot+\$1} END {print tot}'"
 alias avg="awk '{sum+=\$1} END { print sum/NR}'"
+alias utf8tolatin1="iconv -f utf-8 -t iso8859-1"
 
 # SCM
 alias cdiff='colordiff | less -r'
@@ -24,4 +25,3 @@ alias svnignore="svn propedit svn:ignore"
 
 # Typos
 alias gti=git
-
