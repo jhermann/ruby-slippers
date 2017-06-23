52f9e166d56feccf861e564c266b8300f046048a
jhermann <jh@web.de>
2017-06-23 13:31:34 +0200

    explain what is done

--- home/.bashrc.d/zzzz_last.sh
+++ home/.bashrc.d/zzzz_last.sh
@@ -1,4 +1,5 @@
 if test -d ~/bin; then
+    # Make sure ~/bin is in PATH exactly ONCE, in front of everything else
     PATH="$HOME/bin:$(tr : \\n <<<"$PATH" | egrep -v '^$' | egrep -v '^'"$HOME/bin"'$' | tr \\n :)"
     PATH="${PATH%:}"
     export PATH
