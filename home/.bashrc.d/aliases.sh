ce6be5e51b6bbefb80f29edc394852bb4c1769e2
jhermann <jh@web.de>
2018-02-23 17:19:43 +0100

    uc-em-quad: put an em-quad space into the clipboard

--- home/.bashrc.d/aliases.sh
+++ home/.bashrc.d/aliases.sh
@@ -18,6 +18,7 @@ alias total="awk '{tot=tot+\$1} END {print tot}'"
 alias avg="awk '{sum+=\$1} END { print sum/NR}'"
 alias utf8tolatin1="iconv -f utf-8 -t iso8859-1"
 alias sz-tty='wmctrl -r :ACTIVE: -e 0,-1,-1,1470,990; sleep .3; echo "Terminal size set to $(tput cols)×$(tput lines)"'
+alias uc-em-quad="python3 -c 'print(\"\\u2001\", end=\"\")' | xsel -bi"
 
 # SCM
 alias cdiff='colordiff | less -r'
