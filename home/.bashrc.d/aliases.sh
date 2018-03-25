c88ba7b4f7e0a9a081df470ef62cc6bef43baa46
jhermann <jh@web.de>
2018-03-25 20:04:58 +0200

    fix 'eg' config path

--- home/.bashrc.d/aliases.sh
+++ home/.bashrc.d/aliases.sh
@@ -13,7 +13,7 @@ alias xo=xdg-open
 alias notes="pe $HOME/Documents/notes.md"
 
 # Default args
-alias eg="command eg --config-file=$HOME/.local/eg/config.ini"
+alias eg="command eg --config-file=$HOME/.config/eg/config.ini"
 
 # Little helpers
 alias rebashrc=". ~/.bashrc"
