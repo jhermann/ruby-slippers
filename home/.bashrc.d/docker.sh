a58d84a14fd493d80f2db470099d9f125ca02177
jhermann <jh@web.de>
2018-09-20 13:42:07 +0200

    docker: more aliases

--- home/.bashrc.d/docker.sh
+++ home/.bashrc.d/docker.sh
@@ -5,3 +5,20 @@ _docker_run_sock="docker run --rm -it --userns host -v /var/run/docker.sock:/var
 alias ctop="$_docker_run_sock --name=ctop quay.io/vektorlab/ctop"
 alias dockviz="$_docker_run_sock --name=dockviz nate/dockviz"
 unset _docker_run_sock
+
+alias dvi="dockviz images --tree -l --show-created-by"
+
+
+## https://www.calazan.com/docker-cleanup-commands/
+
+# Kill all running containers
+alias docker-kill-all='docker kill $(docker ps -q)'
+
+# Delete all stopped containers
+alias docker-cleanc='printf "\n>>> Deleting stopped containers\n\n" && docker rm $(docker ps -a -q)'
+
+# Delete all untagged images
+alias docker-cleani='printf "\n>>> Deleting untagged images\n\n" && docker rmi $(docker images -q -f dangling=true)'
+
+# Delete all stopped containers and untagged images
+alias docker-clean='( docker-cleanc ; docker-cleani )'
