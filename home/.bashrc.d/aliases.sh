a7cf51bec9764057290f3a0707145f29b1045b8d
jhermann <jh@web.de>
2018-11-08 16:08:33 +0100

    tree alias with default excludes

--- home/.bashrc.d/aliases.sh
+++ home/.bashrc.d/aliases.sh
@@ -10,6 +10,7 @@ alias md=mkdir
 alias ..="cd .."
 alias ...="cd ../.."
 alias xo=xdg-open
+alias tree="tree -I '*~|__pycache__|.git|.venv'"
 alias notes="pe $HOME/Documents/notes.md"
 
 # Define default args
