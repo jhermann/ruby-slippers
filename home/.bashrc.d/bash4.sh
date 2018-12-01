ca01db50d24004c12ffa177eec219399b6c14960
jhermann <jh@web.de>
2018-12-01 09:53:20 +0100

    set a default CDPATH (close #2)

--- home/.bashrc.d/bash4.sh
+++ home/.bashrc.d/bash4.sh
@@ -2,6 +2,11 @@
 HISTSIZE=1000
 HISTFILESIZE=2000
 
+export CDPATH=".:$HOME"
+for homepath in src src/bitbucket src/github src/local Documents Videos Music; do
+    test ! -d "$HOME/$homepath" || export CDPATH="$CDPATH:$HOME/$homepath"
+done
+
 # Protect against failures of bashv4 stuff in older shells
 if test "${BASH_VERSINFO[0]}" -ge 4 ; then
     # If set, the pattern "**" used in a pathname expansion context will
