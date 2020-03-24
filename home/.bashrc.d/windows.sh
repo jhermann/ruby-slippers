aa3d428e5d38547d6b54316d83d73774982a032c
jhermann <jh@web.de>
2020-03-24 08:34:34 +0100

    windows: alias for xdg-open

--- home/.bashrc.d/windows.sh
+++ home/.bashrc.d/windows.sh
@@ -2,6 +2,8 @@ if test -n "$SYSTEMROOT" -a -n "$WINDIR"; then
     # git bash (MingW) environment
     export PS1="${PS1%%\$ *}\$ "
 
+    alias xdg-open=start
+
     test -n "$SSH_AGENT_PID" || eval $(ssh-agent)
     ##ssh-add ~/.ssh/github
 fi
