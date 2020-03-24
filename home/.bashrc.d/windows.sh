da423051d8719b7768d54b2e9a66068d100bc2b6
jhermann <jh@web.de>
2020-03-24 17:17:09 +0100

    windows: set GG_BROWSER

--- home/.bashrc.d/windows.sh
+++ home/.bashrc.d/windows.sh
@@ -1,6 +1,7 @@
 if test -n "$SYSTEMROOT" -a -n "$WINDIR"; then
     # git bash (MingW) environment
     export PS1="${PS1%%\$ *}\$ "
+    export GG_BROWSER=start
 
     alias xdg-open=start
     alias python3="winpty py -3"
