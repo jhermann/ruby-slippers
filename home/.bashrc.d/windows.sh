08bb77286874c8e48214786c59d5eaa89e6652fd
jhermann <jh@web.de>
2020-03-24 08:30:09 +0100

    windows: prompt fix, and ssh agent

--- home/.bashrc.d/windows.sh
+++ home/.bashrc.d/windows.sh
@@ -1,3 +1,7 @@
 if test -n "$SYSTEMROOT" -a -n "$WINDIR"; then
     # git bash (MingW) environment
+    export PS1="${PS1%%\$ *}\$ "
+
+    test -n "$SSH_AGENT_PID" || eval $(ssh-agent)
+    ##ssh-add ~/.ssh/github
 fi
