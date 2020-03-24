51e89aa98b9e00bb7b4fafc38b6a789cce7c0227
jhermann <jh@web.de>
2020-03-24 20:11:09 +0100

    windows: ~/.local/bin and UTF-8

--- home/.bashrc.d/windows.sh
+++ home/.bashrc.d/windows.sh
@@ -1,12 +1,15 @@
 if test -n "$SYSTEMROOT" -a -n "$WINDIR"; then
     # git bash (MingW) environment
     export PS1="${PS1%%\$ *}\$ "
+    export LANG=en_US.UTF-8
     export GG_BROWSER=start
+    prependpathvar PATH "$HOME/.local/bin"
 
     alias xdg-open=start
     alias python3="winpty py -3"
     alias python=python3
 
+    chcp.com 65001 >/dev/null  # use UTF-8 codepage
     test -n "$SSH_AGENT_PID" || eval $(ssh-agent)
     ##ssh-add ~/.ssh/github
 fi
