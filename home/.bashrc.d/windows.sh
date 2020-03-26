ed35031924b1fed14445c95e59da8ff1db49dead
jhermann <jh@web.de>
2020-03-26 17:09:07 +0100

    windows: no space-infested Oracle Java PATH dir

--- home/.bashrc.d/windows.sh
+++ home/.bashrc.d/windows.sh
@@ -1,12 +1,16 @@
 if test -n "$SYSTEMROOT" -a -n "$WINDIR"; then
     # git bash (MingW) environment
+    export PATH=$(tr : \\n <<<"$PATH" | grep -v Oracle/Java | tr \\n :)
+    export PATH="${PATH%:}"
+    ##echo "$PATH" | tr \;\: \\n\\n
+
     export PS1="${PS1%%\$ *}\$ "
     export LANG=en_US.UTF-8
     export PYTHONIOENCODING=UTF-8
     export PYTHONUTF8=1
     export PYTHONPYCACHEPREFIX="$HOME/.local/cache/pycache"
     export GG_BROWSER=start
-    prependpathvar PATH "$HOME/.local/bin"
+    export PATH="$HOME/.local/bin:$PATH"
 
     alias xdg-open=start
     alias python3="winpty py -3"
